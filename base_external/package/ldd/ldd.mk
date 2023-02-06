081c87cfb4d6c3d743c885d5afc4e30747ba7dd3
Kenneth Hunter <kennethdhunter@gmail.com>
2023-02-05 20:52:05 -0700

    Finalized for assignment 7 with passing tests

---
+++ base_external/package/ldd/ldd.mk
@@ -0,0 +1,20 @@
+
+##############################################################
+#
+# LDD3 
+#
+##############################################################
+
+LDD_VERSION = 'c0d72b010f7f68576de79ef48c5847a01aec7381'
+# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
+# with ssh keys and the automated build/test system.
+# Your site should start with git@github.com:
+LDD_SITE = git@github.com:cu-ecen-aeld/assignment-7-ichbinkenny.git
+LDD_SITE_METHOD = git
+LDD_GIT_SUBMODULES = YES
+
+LDD_MODULE_SUBDIRS = misc-modules
+LDD_MODULE_SUBDIRS += scull
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
